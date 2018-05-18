df1ed0806f2f5df9c205351b5ae3380eda6c2670
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-05-17 22:35:35 -0300

    Teclado inline (botões)

---
+++ curso-bot/exercicios/botoes.js
@@ -0,0 +1,49 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+
+let contagem = 0
+
+const botoes = Extra.markup(Markup.inlineKeyboard([
+    Markup.callbackButton('+1', 'add 1'),
+    Markup.callbackButton('+10', 'add 10'),
+    Markup.callbackButton('+100', 'add 100'),
+    Markup.callbackButton('-1', 'sub 1'),
+    Markup.callbackButton('-10', 'sub 10'),
+    Markup.callbackButton('-100', 'sub 100'),
+    Markup.callbackButton('🔃 Zerar', 'reset'),
+    Markup.callbackButton('Resultado', 'result')
+], {columns: 3}))
+
+bot.start(async ctx => {
+    const nome = ctx.update.message.from.first_name
+    await ctx.reply(`Seja bem vindo ${nome}!`)
+    await ctx.reply(`A contagem atual está em ${contagem}`, botoes)
+})
+
+
+//   /add (\d+)/gi para capturar todos /add com d+ sendo um número]
+// gi onde g é o global e o i é case sensitive
+bot.action(/add (\d+)/gi, ctx => {
+    contagem += parseInt(ctx.match[1])
+    ctx.reply(`A contagem atual está em ${contagem}`, botoes)
+})
+
+bot.action(/sub (\d+)/gi, ctx => {
+    contagem -= parseInt(ctx.match[1])
+    ctx.reply(`A contagem atual está em ${contagem}`, botoes)
+})
+
+bot.action('reset', ctx => {
+    contagem = 0
+    ctx.reply(`A contagem atual está em ${contagem}`, botoes)
+})
+
+bot.action('result', ctx => {
+    ctx.answerCbQuery(`A contagem atual está em ${contagem}`)
+})
+
+
+bot.startPolling()
